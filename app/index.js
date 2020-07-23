8e130d850b5b3058878f61cc31b2b925eb026219
ccrisolo <ccrisolo3@gmail.com>
2020-07-23 12:22:24 -0700

    add animation to fade out sign in button

--- app/index.js
+++ app/index.js
@@ -1,8 +1,57 @@
 import React, { Component } from "react";
 import { View, Text, StyleSheet, Image, Dimensions } from "react-native";
+import Animated, { Easing } from "react-native-reanimated";
+import { TapGestureHandler, State } from "react-native-gesture-handler";
 
 const { width, height } = Dimensions.get("window");
+
+const { Value, event, block, cond, eq, set, Clock, startClock, stopClock, debug, timing, clockRunning } = Animated;
+
+//sets animation state
+function runTiming(clock, value, dest) {
+  const state = {
+    finished: new Value(0),
+    position: new Value(0),
+    time: new Value(0),
+    frameTime: new Value(0),
+  };
+//configures the animation
+  const config = {
+    duration: 1000,
+    toValue: new Value(0),
+    easing: Easing.inOut(Easing.ease),
+  };
+//runs the animation
+  return block([
+    cond(clockRunning(clock), 0, [
+      set(state.finished, 0),
+      set(state.time, 0),
+      set(state.position, value),
+      set(state.frameTime, 0),
+      set(config.toValue, dest),
+      startClock(clock),
+    ]),
+    timing(clock, state, config),
+    cond(state.finished, debug("stop clock", stopClock(clock))),
+    state.position,
+  ]);
+}
+
 class MusicApp extends Component {
+  constructor() {
+    super();
+    // set up animated value
+    this.buttonOpacity = new Value(1);
+
+    this.onStateChange = event([
+      {
+        nativeEvent: ({ state }) =>
+          block([
+            cond(eq(state, State.END), set(this.buttonOpacity, runTiming(new Clock(), 1, 0 )))
+          ]),
+      },
+    ]);
+  }
   render() {
     return (
       <View
@@ -18,12 +67,17 @@ class MusicApp extends Component {
             style={{ flex: 1, height: null, width: null }}
           />
         </View>
-        <View style={{ height: height / 3, justifyContent: 'center' }}>
-          <View style={styles.button}>
-            <Text style={{ fontSize: 20, fontWeight: "bold" }}>SIGN IN</Text>
-          </View>
-          <View style={{...styles.button, backgroundColor: '#2E71DC'}}>
-            <Text style={{ fontSize: 20, fontWeight: "bold", color: 'white'}}>
+        <View style={{ height: height / 3, justifyContent: "center" }}>
+          {/* TapGestureHandler makes view pressable/clickable. has an onHandlerStateChange prop */}
+          <TapGestureHandler onHandlerStateChange={this.onStateChange}>
+            <Animated.View
+              style={{ ...styles.button, opacity: this.buttonOpacity }}
+            >
+              <Text style={{ fontSize: 20, fontWeight: "bold" }}>SIGN IN</Text>
+            </Animated.View>
+          </TapGestureHandler>
+          <View style={{ ...styles.button, backgroundColor: "#2E71DC" }}>
+            <Text style={{ fontSize: 20, fontWeight: "bold", color: "white" }}>
               SIGN IN WITH FACEBOOK
             </Text>
           </View>
