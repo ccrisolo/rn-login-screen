c9f7ad3a1e0aaff106ebbd384b742ede9d2834f8
ccrisolo <ccrisolo3@gmail.com>
2020-07-23 13:44:26 -0700

    add vertical slide animation to log in screen

--- app/index.js
+++ app/index.js
@@ -1,11 +1,25 @@
 import React, { Component } from "react";
 import { View, Text, StyleSheet, Image, Dimensions } from "react-native";
-import Animated, { Easing } from "react-native-reanimated";
+import Animated, { Easing, Extrapolate } from "react-native-reanimated";
 import { TapGestureHandler, State } from "react-native-gesture-handler";
 
 const { width, height } = Dimensions.get("window");
 
-const { Value, event, block, cond, eq, set, Clock, startClock, stopClock, debug, timing, clockRunning } = Animated;
+const {
+  Value,
+  event,
+  block,
+  cond,
+  eq,
+  set,
+  Clock,
+  startClock,
+  stopClock,
+  debug,
+  timing,
+  clockRunning,
+  interpolate,
+} = Animated;
 
 //sets animation state
 function runTiming(clock, value, dest) {
@@ -15,13 +29,13 @@ function runTiming(clock, value, dest) {
     time: new Value(0),
     frameTime: new Value(0),
   };
-//configures the animation
+  //configures the animation
   const config = {
     duration: 1000,
     toValue: new Value(0),
     easing: Easing.inOut(Easing.ease),
   };
-//runs the animation
+  //runs the animation
   return block([
     cond(clockRunning(clock), 0, [
       set(state.finished, 0),
@@ -47,10 +61,26 @@ class MusicApp extends Component {
       {
         nativeEvent: ({ state }) =>
           block([
-            cond(eq(state, State.END), set(this.buttonOpacity, runTiming(new Clock(), 1, 0 )))
+            cond(
+              eq(state, State.END),
+              set(this.buttonOpacity, runTiming(new Clock(), 1, 0))
+            ),
           ]),
       },
     ]);
+
+    this.buttonY = interpolate(this.buttonOpacity, {
+      inputRange: [0, 1],
+      outputRange: [100, 0],
+      extrapolate: Extrapolate.CLAMP,
+    });
+
+    this.bgY = interpolate(this.buttonOpacity, {
+      inputRange: [0, 1],
+      //below slides the screen up 1/3 of the height
+      outputRange: [-height / 3, 0],
+      extrapolate: Extrapolate.CLAMP,
+    });
   }
   render() {
     return (
@@ -61,26 +91,37 @@ class MusicApp extends Component {
           justifyContent: "flex-end",
         }}
       >
-        <View style={{ ...StyleSheet.absoluteFill }}>
+        <Animated.View style={{ ...StyleSheet.absoluteFill, transform:[{ translateY: this.bgY }] }}>
           <Image
             source={require("../assets/earthBG.jpg")}
             style={{ flex: 1, height: null, width: null }}
           />
-        </View>
+        </Animated.View>
         <View style={{ height: height / 3, justifyContent: "center" }}>
           {/* TapGestureHandler makes view pressable/clickable. has an onHandlerStateChange prop */}
           <TapGestureHandler onHandlerStateChange={this.onStateChange}>
             <Animated.View
-              style={{ ...styles.button, opacity: this.buttonOpacity }}
+              style={{
+                ...styles.button,
+                opacity: this.buttonOpacity,
+                transform: [{ translateY: this.buttonY }],
+              }}
             >
               <Text style={{ fontSize: 20, fontWeight: "bold" }}>SIGN IN</Text>
             </Animated.View>
           </TapGestureHandler>
-          <View style={{ ...styles.button, backgroundColor: "#2E71DC" }}>
+          <Animated.View
+            style={{
+              ...styles.button,
+              backgroundColor: "#2E71DC",
+              opacity: this.buttonOpacity,
+              transform: [{ translateY: this.buttonY }],
+            }}
+          >
             <Text style={{ fontSize: 20, fontWeight: "bold", color: "white" }}>
               SIGN IN WITH FACEBOOK
             </Text>
-          </View>
+          </Animated.View>
         </View>
       </View>
     );
